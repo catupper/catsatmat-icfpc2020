472094259dc280c3d41b435816a86671677801ae
松下祐介 <y.skm24t@gmail.com>
2020-07-18 03:34:30 +0900

    parse

---
+++ app/lib.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+/// 式
+#[derive(Debug)]
+pub enum Expr {
+  Ap(Box<Expr>, Box<Expr>),
+  Int(i32),
+  Cst(String),
+  Def(i32),
+}
+use Expr::*;
+
+/// パースする
+/// galaxy は -1 扱い
+pub fn parse(text: &str) -> HashMap<i32, Expr> {
+  let mut map = HashMap::new();
+  for line in text.lines() {
+    let mut words = line.split_whitespace();
+    let head = words.next().unwrap();
+    let def_n = if head == "galaxy" {
+      -1
+    } else {
+      head.strip_prefix(":").unwrap().parse::<i32>().unwrap()
+    };
+    assert_eq!(words.next().unwrap(), "=");
+    let mut stack = Vec::new();
+    let words = words.rev();
+    for word in words {
+      match word.parse::<i32>() {
+        Ok(n) => {
+          stack.push(Int(n));
+        }
+        Ng => match word.strip_prefix(":") {
+          Some(sn) => {
+            stack.push(Def(sn.parse::<i32>().unwrap()));
+          }
+          None => {
+            if word == "ap" {
+              let e1 = stack.pop().unwrap();
+              let e2 = stack.pop().unwrap();
+              stack.push(Ap(Box::new(e1), Box::new(e2)));
+            } else {
+              stack.push(Cst(word.to_owned()));
+            }
+          }
+        },
+      }
+    }
+    let e = stack.pop().unwrap();
+    assert!(stack.is_empty());
+    map.insert(def_n, e);
+  }
+  map
+}
--- app/main.rs
+++ app/main.rs
@@ -1,7 +1,9 @@
 use http_body::Body as _;
-use hyper::{Client, Request, Method, Body, StatusCode};
+use hyper::{Body, Client, Method, Request, StatusCode};
 use std::env;
 use std::process;
+
+mod lib;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -10,33 +12,30 @@ async fn sample(server_url: &str, player_key: &str) -> Result<()> {
     let req = Request::builder()
         .method(Method::POST)
         .uri(server_url)
-    .body(Body::from(player_key.to_string()))?;
-
+        .body(Body::from(player_key.to_string()))?;
 
     match client.request(req).await {
-        Ok(mut res) => {
-            match res.status() {
-                StatusCode::OK => {
-                    print!("Server response: ");
-                    while let Some(chunk) = res.body_mut().data().await {
-                        match chunk {
-                            Ok(content) => println!("{:?}", content),
-                            Err(why) => println!("error reading body: {:?}", why)
-                        }
+        Ok(mut res) => match res.status() {
+            StatusCode::OK => {
+                print!("Server response: ");
+                while let Some(chunk) = res.body_mut().data().await {
+                    match chunk {
+                        Ok(content) => println!("{:?}", content),
+                        Err(why) => println!("error reading body: {:?}", why),
                     }
-                },
-                _ => {
-                    println!("Unexpected server response:");
-                    println!("HTTP code: {}", res.status());
-                    print!("Response body: ");
-                    while let Some(chunk) = res.body_mut().data().await {
-                        match chunk {
-                            Ok(content) => println!("{:?}", content),
-                            Err(why) => println!("error reading body: {:?}", why)
-                        }
-                    }
-                    process::exit(2);
                 }
+            }
+            _ => {
+                println!("Unexpected server response:");
+                println!("HTTP code: {}", res.status());
+                print!("Response body: ");
+                while let Some(chunk) = res.body_mut().data().await {
+                    match chunk {
+                        Ok(content) => println!("{:?}", content),
+                        Err(why) => println!("error reading body: {:?}", why),
+                    }
+                }
+                process::exit(2);
             }
         },
         Err(err) => {
@@ -52,8 +51,8 @@ async fn aliens(server_url: &str, player_key: &str) -> Result<()> {
     let req = Request::builder()
         .method(Method::POST)
         .uri(server_url.to_string() + "/aliens/send")
-    .body(Body::from(player_key.to_string()))?;
-    client.request(req).await?; 
+        .body(Body::from(player_key.to_string()))?;
+    client.request(req).await?;
     Ok(())
 }
 
@@ -68,6 +67,6 @@ async fn main() -> Result<()> {
 
     aliens(server_url, player_key).await?;
     sample(server_url, player_key).await?;
-    
+
     Ok(())
 }
