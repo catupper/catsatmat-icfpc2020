348d23d58d2ac78b6530aeb8503e69ba49f30771
Ken Ogura <nekarugo628@gmail.com>
2020-07-18 03:43:17 +0900

    good

--- app/main.rs
+++ app/main.rs
@@ -1,9 +1,15 @@
 use http_body::Body as _;
-use hyper::{Client, Request, Method, Body, StatusCode};
+use hyper::{Client, Request, Method, Body, StatusCode, Response};
 use std::env;
 use std::process;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+const API_KEY: &str = "41ff8e29e5fa4596928186fcfe5bfee2";
+
+async fn print_response(response: Result<Response<Body>>)->Result<()>{
+    Ok(())
+}
 
 async fn sample(server_url: &str, player_key: &str) -> Result<()> {
     let client = Client::new();
@@ -13,17 +19,32 @@ async fn sample(server_url: &str, player_key: &str) -> Result<()> {
     .body(Body::from(player_key.to_string()))?;
 
 
-    match client.request(req).await {
+    Ok(())
+}
+
+async fn aliens(server_url: &str, request_string: String) -> Result<String> {
+    let client = Client::new();
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(server_url.to_string() + "/aliens/send?apiKey=" + API_KEY)
+        .body(Body::from(request_string))?;
+
+    match client.request(req).await{
         Ok(mut res) => {
             match res.status() {
                 StatusCode::OK => {
                     print!("Server response: ");
+                    let mut response = "".to_string();
                     while let Some(chunk) = res.body_mut().data().await {
                         match chunk {
-                            Ok(content) => println!("{:?}", content),
+                            Ok(content) => {
+                                response = response + &String::from_utf8(content.to_vec()).unwrap();                                    
+                                println!("{:?}", content)
+                            },
                             Err(why) => println!("error reading body: {:?}", why)
                         }
                     }
+                    Ok(response)
                 },
                 _ => {
                     println!("Unexpected server response:");
@@ -44,17 +65,6 @@ async fn sample(server_url: &str, player_key: &str) -> Result<()> {
             process::exit(1);
         }
     }
-    Ok(())
-}
-
-async fn aliens(server_url: &str, player_key: &str) -> Result<()> {
-    let client = Client::new();
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(server_url.to_string() + "/aliens/send")
-    .body(Body::from(player_key.to_string()))?;
-    client.request(req).await?; 
-    Ok(())
 }
 
 #[tokio::main]
@@ -66,8 +76,7 @@ async fn main() -> Result<()> {
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
 
-    aliens(server_url, player_key).await?;
-    sample(server_url, player_key).await?;
-    
+    let response = aliens(server_url, "01010101".to_string()).await?;
+    print!("{}", response);    
     Ok(())
 }
