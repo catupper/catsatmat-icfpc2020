a0ab3d604dcfa4e5a3c7775ee31b6aefb73842be
Ken Ogura <nekarugo628@gmail.com>
2020-07-19 15:46:10 +0900

    add many

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod board;
 mod cons;
 pub mod data;
 mod expr;
@@ -5,6 +6,7 @@ mod interpret;
 mod parse;
 mod send_alien;
 
+pub use board::*;
 pub use cons::*;
 pub use expr::*;
 pub use interpret::*;
